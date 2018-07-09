39a0c198eccb73d26365d6a5bb22797d47cb7ae7
dchengs <developer.chengs@gmail.com>
2018-07-10 00:18:39 +0800

    Minor fixes

--- Handler.go
+++ Handler.go
@@ -4,33 +4,33 @@ import (
 	"fmt"
 	"html/template"
 	"net/http"
+
 	"./Utility"
 )
 
 //list of pages handler
 func Index(w http.ResponseWriter, r *http.Request) {
-	index, err := template.ParseFiles("index.html")
-	if err != nil{ 
+	index, err := template.ParseFiles("html/index.html")
+	if err != nil {
 		Utility.ErrLogger(err)
 	}
 	fmt.Println(index.Execute(w, nil))
 }
 
 func CurrentProject(w http.ResponseWriter, r *http.Request) {
-	currentProject, err := template.ParseFiles("currentProject.html")
-	if err != nil{
+	currentProject, err := template.ParseFiles("html/currentProject.html")
+	if err != nil {
 		Utility.ErrLogger(err)
 	}
 	currentProject.Execute(w, nil)
 }
 func AboutMe(w http.ResponseWriter, r *http.Request) {
-	aboutMe, err := template.ParseFiles("aboutMe.html")
-	if err != nil{
+	aboutMe, err := template.ParseFiles("html/aboutMe.html")
+	if err != nil {
 		Utility.ErrLogger(err)
 	}
 	aboutMe.Execute(w, nil)
 }
-
 
 func main() {
 	http.HandleFunc("/", Index)
