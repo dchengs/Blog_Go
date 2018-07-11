3f4e221c116b8499a7376bf72d36e723a15f3144
dchengs <developer.chengs@gmail.com>
2018-07-11 19:31:21 +0800

    fixed logger crash when directory exists

--- Utility/logger.go
+++ Utility/logger.go
@@ -7,15 +7,20 @@ import (
 
 //logging
 func ErrLogger(errMessage error) {
-	//make log directory if it doesn't exist
-	err := os.Mkdir("logs", 0755)
-	if err != nil {
-		log.Fatalln("Problem creating path %v", err)
+	//Stat performs simple look up of the directory
+	_, err := os.Stat("logs") 
+	//checking if directory exists
+	if os.IsNotExist(err){
+		//creates logs directory
+		err := os.Mkdir("logs", 0755)
+		if err !=nil{
+			log.Fatalln("Problem creating logs directory %s", err)
+		}
 	}
 	//check if log.txt is opened, else create one
 	logFile, err := os.OpenFile("logs/log.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 	if err != nil {
-		log.Fatalln("Problem opening the file %v", err)
+		log.Fatalln("Problem opening the file %s", err)
 	}
 	//ensure the file closes
 	defer logFile.Close()
