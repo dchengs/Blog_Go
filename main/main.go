bad348b10293e1685290efa81e029fee49163214
dchengs <developer.chengs@gmail.com>
2018-07-25 22:19:07 +0800

    Minor fixes and added 404 Not Found

--- main/main.go
+++ main/main.go
@@ -1,91 +1,100 @@
 package main
 
 import (
+	"Blog_go/utilities/logger"
 	"fmt"
 	"html/template"
 	"net/http"
-	"crypto/sha256"
-	"./logger"
 )
 
 //list of pages handler
 func Index(w http.ResponseWriter, r *http.Request) {
-	template, err := template.ParseFiles("html/index.html")
-	if err != nil {
-		l.ErrLogger(err)
-	}
-	else{
-		template.Execute(w, nil)
+	//shows 404 not found
+	if r.URL.Path != "/" {
+		NotFound(w, r)
+	} else {
+		template, err := template.ParseFiles("../htmls/index.html")
+		if err != nil {
+			logger.ErrLogger(err)
+		} else {
+			template.Execute(w, nil)
+		}
 	}
 }
 
 func Project(w http.ResponseWriter, r *http.Request) {
-	Project, err := template.ParseFiles("html/currentProject.html")
+	template, err := template.ParseFiles("../htmls/currentProject.html")
 	if err != nil {
-		l.ErrLogger(err)
-	}
-	else{
+		logger.ErrLogger(err)
+	} else {
 		template.Execute(w, nil)
 	}
 }
 func AboutMe(w http.ResponseWriter, r *http.Request) {
-	template, err := template.ParseFiles("html/aboutMe.html")
+	template, err := template.ParseFiles("../htmls/aboutMe.html")
 	if err != nil {
-		l.ErrLogger(err)
-	}
-	else{
+		logger.ErrLogger(err)
+	} else {
 		template.Execute(w, nil)
 	}
 }
 
-
-func Resume(w http.ResponseWriter, r *http.Request){
-	template, err := template.ParseFiles("html/resume.html")
-	if err != nil{
-		l.ErrLogger(err)
-	}
-	else{
+func Resume(w http.ResponseWriter, r *http.Request) {
+	template, err := template.ParseFiles("../htmls/resume.html")
+	if err != nil {
+		logger.ErrLogger(err)
+	} else {
 		template.Execute(w, nil)
 	}
 
 }
 
-func Login(w http.ResponseWriter, r *http.Request){
+func Login(w http.ResponseWriter, r *http.Request) {
 	//user requests the login page
-	if(r.Method() == "GET"){
-		template, err = := template.ParseFiles("html/login.html")
-		template.execute(w, nil)
-		if err != nil{
-			ult.ErrLogger(err)
+
+	if r.Method == "GET" {
+		template, err := template.ParseFiles("../htmls/login.html")
+		if err != nil {
+			logger.ErrLogger(err)
 		}
-	}
-	else{
+		template.Execute(w, nil)
+	} else {
 		//simple debug testing
 		debugUsername := "chengs"
 		debugPassword := "123123123"
-		username:=r.Form["username"]
-		password:= r.Form["password"]
-		if (debugUsername == username && debugPassword == password){
+		r.ParseForm()
+		username := r.PostFormValue("username")
+		password := r.PostFormValue("password")
+		if debugUsername == username && debugPassword == password {
 			fmt.Println("Correct password")
-		}
-		else{
+		} else {
 			fmt.Println("Incorrect password")
 		}
-		//encrypt password input
-		sha_256 := sha256.New()
-		//convert password to byte array
-		sha_256.Write([]byte(password))
-		encryptedPW := hex.EncodeToString(sha_256.Sum(nil))
-		//check with server
+		/*
+			//encrypt password input
+			sha_256 := sha256.New()
+			//convert password to byte array
+			sha_256.Write([]byte(password))
+			encryptedPW := hex.EncodeToString(sha_256.Sum(nil))
+			//check with serve
+		*/
 
+	}
+}
+func NotFound(w http.ResponseWriter, r *http.Request) {
+	template, err := template.ParseFiles("../htmls/404.html")
+	if err != nil {
+		logger.ErrLogger(err)
+	} else {
+		template.Execute(w, nil)
 	}
 }
 
 func main() {
 	http.HandleFunc("/", Index)
-	http.HandleFunc("/Project/", Project)
-	http.HandleFunc("/AboutMe/", AboutMe)
-	http.HandleFunc("/login", auth.login)
-	http.HandleFunc("Resume", Resume)
+	http.HandleFunc("/project/", Project)
+	http.HandleFunc("/aboutMe/", AboutMe)
+	http.HandleFunc("/login/", Login)
+	http.HandleFunc("/resume/", Resume)
 	http.ListenAndServe(":5000", nil)
 }
